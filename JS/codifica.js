9e03bfaff4d710ba87e7f495c718e774fe2cd864
Izadora Ferreira <izadorasantos20@hotmail.com>
2022-02-25 17:03:02 -0300

    base64

--- JS/codifica.js
+++ JS/codifica.js
@@ -1,21 +1,66 @@
 let codificar = document.querySelector('#codificar');
 let decodificar = document.querySelector('#decodificar');
-let action = document.querySelector('button');
+let acao = document.querySelector('#submit');
+let mensagem = document.querySelector('#message');
+let resposta = document.querySelector('#response');
+let select = document.querySelector('#selecione');
 
 //mudar botões codificar/decodificar
 codificar.onclick = function(){
-    action.innerHTML = "Codificar mensagem";
+    acao.innerHTML = "Codificar mensagem";
     decodificar.disabled = true;
 }
 
 decodificar.onclick = function(){
-    action.innerHTML = "Decodificar mensagem";
+    acao.innerHTML = "Decodificar mensagem";
     codificar.disabled = true;
 }
 
+//base64:
+function codificaMsg (){
+    let textoCodifica = message.value;
+    let Codeb64 = btoa(textoCodifica);
+
+    if (textoCodifica === " "){
+        resposta.innerHTML = `Digite uma mensagem`;
+    } else {
+        resposta.innerHTML = Codeb64;
+    }
+    return resposta
+}
+
+function decodificaMsg(){
+    let textoDecodifica = atob(resposta.value);
+
+    if(textoDecodifica === " "){
+        resposta.innerHTML = `Digite uma mensagem codificada`;
+    } else {
+        resposta.innerHTML = textoDecodifica;
+    }
+}
+
+function btnCodDecod(event){
+    event.preventDefault()
+
+    let checkCod = codificar.checked;
+    let checkDecod = decodificar.checked;
+
+    if(select.options[select.selectedIndex].value === "base" && checkCod){
+        codificaMsg()
+        decodificar.disabled = false;
+    } else if (select.options[select.selectedIndex].value === "base" && checkDecod){
+        decodificaMsg()
+        codificar.disabled = false;
+    } else{
+        alert (`Insira uma mensagem válida :D`)
+    }
+}
+
+acao.addEventListener("click", btnCodDecod)
+
+
 //incremento da cifra
-let selecione = document.querySelector('#selecione');
-let cifra = document.querySelector('#cifra');
+/*let cifra = document.querySelector('#cifra');
 let base = document.querySelector('#base')
 let incremento = document.querySelector('#incremento');
 
@@ -25,14 +70,4 @@ selecione.addEventListener("change", function(){
     } else {
         incremento1.style.display = "none"
     }
-});
-
-
-
-
-
-
-
-
-
-
+});*/
